e6b069b11d0ce420609f50be7fb811e1d88b80bf
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-01-09 15:36:55 -0500

    feat: basic functionality loop and timing/speech test

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import pyttsx3
 import platform
 import obd
+import time
+
+
+
+
 
 engine = pyttsx3.init()
+simMode = platform.system() == 'Windows';
 
-if(platform.system() == 'Windows'):
+
+if(simMode):
     engine.say("Windows.") #debug statement
+    engine.runAndWait()
     #windows 'mode' is essentially a simulated test case rather than live use.
-elif(platform.system() == 'Linux'):
+else:
     engine.say("Active. Initiating Connection")
     engine.runAndWait()
     connection = obd.OBD()
     #linux 'mode' assumed live use.
-    if(connectin.is_connected()):
+    if(connection.is_connected()):
         engine.say("Connection successful.")
+    engine.runAndWait()
 
-engine.runAndWait()
+i = 0
 
+while(True):
+    
+    if(i % 60 == 0):
+        print("temp")
+        engine.say("Temp Check")
+        engine.runAndWait()
+        
+    
+    if(i % 10 == 0):
+        print("speed")
+        engine.say("Speed Check")
+        engine.runAndWait()
+    
+    time.sleep(1)
+    i += 1
