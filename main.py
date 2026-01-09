a6af454884115b30db3941852309603e25b5a12b
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-01-09 14:56:35 -0500

    feat: initial os check for simulated vs live use case

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pyttsx3
+import platform
+import obd
 
 engine = pyttsx3.init()
 
-engine.say("Audio test.")
+if(platform.system() == 'Windows'):
+    engine.say("Windows.") #debug statement
+    #windows 'mode' is essentially a simulated test case rather than live use.
+elif(platform.system() == 'Linux'):
+    engine.say("Active. Initiating Connection")
+    engine.runAndWait()
+    connection = obd.OBD()
+    #linux 'mode' assumed live use.
+    if(connectin.is_connected()):
+        engine.say("Connection successful.")
 
 engine.runAndWait()
+
